f3f5ddc8fce293a7b46a1128212f04c4868b700b
Simon Thompson <simonthompson99@googlemail.com>
2020-03-13 14:42:40 +0000

    add in trigger functions to create archive tables

--- sql_scripts/after_table_creation.sql
+++ sql_scripts/after_table_creation.sql
@@ -17,3 +17,52 @@ add constraint only_one_non_null check (
     = 1
 );
 
+/* trigger function to copy row into archives table */
+/* metrics table */
+/* function */
+create table nhse_contractural_metrics.metric_archive as table nhse_contractural_metrics.metric;
+alter table nhse_contractural_metrics.metric_archive owner to cdt_user;
+create or replace function nhse_contractural_metrics.metric_archive_f ()
+returns trigger
+as
+$$
+begin
+insert into nhse_contractural_metrics.metric_archive
+    (
+        uid,
+        identifier_uid,
+        type_cid,
+        value_cid,
+        value_integer,
+        value_numeric,
+        value_boolean,
+        value_string,
+        value_datetime,
+        de_datetime
+    )
+    values
+    (
+        new.uid,
+        new.identifier_uid,
+        new.type_cid,
+        new.value_cid,
+        new.value_integer,
+        new.value_numeric,
+        new.value_boolean,
+        new.value_string,
+        new.value_datetime,
+        new.de_datetime
+    );
+return new;
+end;
+$$
+language plpgsql;
+alter function nhse_contractural_metrics.metric_archive_f owner to cdt_user;
+/* trigger */
+create trigger metric_archive_t
+after insert
+on nhse_contractural_metrics.metric
+for each row
+execute procedure nhse_contractural_metrics.metric_archive_f();
+
+
