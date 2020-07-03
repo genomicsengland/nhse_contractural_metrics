c66f70f731f16bacdc1877ad2372c6325b36121f
acoe <andrewdcoe@gmail.com>
2020-07-03 15:21:29 +0100

    Update metric/s006.py
    Co-authored-by: Simon Thompson <simon.thompson@genomicsengland.co.uk>

--- metric/s006.py
+++ metric/s006.py
@@ -16,14 +16,9 @@ def get_data(s):
     source data and return list of dictionaries
     """
 
-    q = s.query(gr.referral_participant.patient_uid,
-                gr.concept.concept_code.label('metric')
+    q = s.query(gr.Referral.uid,
+                gr.Concept.concept_code.label('metric')
     ).\
-    
-    join(gr.referral,
-             gr.referral_participant.referral_uid ==
-             gr.referral.uid)
-    
     join(gr.concept,
              gr.referral.priority_cid ==
              gr.concept.uid)
